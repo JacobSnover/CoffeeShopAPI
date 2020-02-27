791f48e932c55c0c3c06332d37cd833df59c89ed
Jacob R. Snover <snoverjacob@yahoo.com>
2020-02-26 19:50:38 -0500

    updating api

--- CoffeeShopAPI/Controllers/CoffeeShopController.cs
+++ CoffeeShopAPI/Controllers/CoffeeShopController.cs
@@ -33,18 +33,29 @@ namespace CoffeeShopAPI.Controllers
         [Route("[action]")]
         public void AddItems(string item)
         {
+            // made db context into using statement
             using (var db = new CoffeeShopContext())
             {
+                // wrapped our database actions in a transaction
                 using (var scope = db.Database.BeginTransaction())
                 {
-                    var Item = JsonSerializer.Deserialize<Items>(item);
-                    Item.id = 0;
-                    db.Items.Add(Item);
-                    db.SaveChanges();
-                    scope.Rollback();
+                    try
+                    {
+                        var Item = JsonSerializer.Deserialize<Items>(item);
+                        Item.id = 0;
+                        db.Items.Add(Item);
+                        db.SaveChanges();
+
+                        // run commit to SaveChanges made to the database
+                        scope.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        //if we run rollback we can cancel all database actions
+                        scope.Rollback();
+                    }
                 }
             }
-
         }
 
         [HttpGet]
@@ -57,13 +68,22 @@ namespace CoffeeShopAPI.Controllers
                 {
                     try
                     {
-                        var Item = db.Items.Where(item => item.id == Convert.ToInt32(itemID)).SingleOrDefault();
+                        // use a Linq where statement to find the Item you need
+                        //I also call SingleOrDefault this way if there is no match in the DB
+                        //it will return a default object instead
+                        var Item = db.Items
+                            .Where(item => item.id == Convert.ToInt32(itemID))
+                            .SingleOrDefault();
+
+                        //pass in the object to be removed
                         db.Items.Remove(Item);
                         db.SaveChanges();
+                        //call commit to SaveChanges
                         scope.Commit();
                     }
                     catch (Exception ex)
                     {
+                        //call rollback to cancel all DB actions
                         scope.Rollback();
                     }
                    
